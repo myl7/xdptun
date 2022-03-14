dd43542af64a3e4839ecc7a889963a7060345026
myl7 <myl@myl.moe>
2022-03-14 09:29:46 +0800

    Fix ingress to remove duplicated checksum and update final action

--- src/bpf/ingress.c
+++ src/bpf/ingress.c
@@ -73,12 +73,9 @@ int ingress_f(struct xdp_md *ctx) {
     return XDP_PASS;
   }
 
-  // Update UDP header checksum
-  udp->check = bpf_htons(csum_delta(tcp_check, IPPROTO_UDP - IPPROTO_TCP - 12 - 5));
-
   memmove((void *)tcp + 4, data_bak, 12);
   bpf_xdp_adjust_tail(ctx, -12);
 
   LOG_INFO("ingress done");
-  return XDP_TX;
+  return XDP_PASS;
 }
