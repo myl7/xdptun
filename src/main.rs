89dc871b4af28807d2be1eaaf7424b6a44a22517
myl7 <myl@myl.moe>
2023-01-23 22:16:33 +0800

    Add XDP ingress cli control

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // Copyright (C) 2023 myl7
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+use std::{fmt, ptr};
+
 use clap::{Parser, ValueEnum};
-use libbpf_rs::{TcHookBuilder, TC_EGRESS};
+use libbpf_rs::{libbpf_sys, TcHookBuilder, TC_EGRESS};
 
 mod egress {
     include!(concat!(env!("OUT_DIR"), "/egress.skel.rs"));
@@ -56,9 +58,45 @@ fn main() -> anyhow::Result<()> {
                 .unwrap()
                 .load()
                 .unwrap();
-            let _fd = skel.progs().ingress_f().fd();
+            let fd = skel.progs().ingress_f().fd();
 
-            todo!("ingress")
+            let in_mode = match cli.inmode {
+                InMode::HW => libbpf_sys::XDP_FLAGS_HW_MODE,
+                InMode::DRV => libbpf_sys::XDP_FLAGS_DRV_MODE,
+                InMode::SKB => libbpf_sys::XDP_FLAGS_SKB_MODE,
+            };
+            {
+                use libbpf_sys::*;
+                match cli.act {
+                    // TODO: libbpf-rs has not wrap XDP API, so we use libbpf-sys directly
+                    // TODO: 3 modes
+                    Act::Attach => unsafe {
+                        // Safety: Only use ifidx, fd from out, which are all int
+                        let flags = XDP_FLAGS_UPDATE_IF_NOEXIST | in_mode;
+                        let err = bpf_xdp_attach(ifidx, fd, flags, ptr::null());
+                        if err < 0 {
+                            anyhow::bail!("Fail to attach: Code {}", err)
+                        }
+                    },
+                    Act::Detach => unsafe {
+                        let err = bpf_xdp_detach(ifidx, in_mode, ptr::null());
+                        if err < 0 {
+                            anyhow::bail!("Fail to detach: Code {}", err)
+                        }
+                    },
+                    Act::Query => unsafe {
+                        let mut id: u32 = 0;
+                        let err = bpf_xdp_query_id(ifidx, in_mode as i32, &mut id);
+                        if err < 0 {
+                            anyhow::bail!("No hook program: Code {}", err)
+                        }
+                        if id == 0 {
+                            anyhow::bail!("No hook program")
+                        }
+                        println!("Hook program ID: {}", id);
+                    },
+                };
+            }
         }
     };
     Ok(())
@@ -74,6 +112,10 @@ struct Cli {
     act: Act,
     /// Network interface
     iface: String,
+
+    /// Ingress XDP attach mode
+    #[clap(short = 'm', long, default_value_t = InMode::DRV)]
+    inmode: InMode,
 }
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Direction {
@@ -85,4 +127,22 @@ enum Act {
     Attach,
     Detach,
     Query,
+}
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum InMode {
+    /// Hardware offload (UNSUPPORTED currently)
+    HW,
+    /// Native mode relying on driver support
+    DRV,
+    /// Simulate after SKB allocation
+    SKB,
+}
+impl fmt::Display for InMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InMode::HW => write!(f, "hw"),
+            InMode::DRV => write!(f, "drv"),
+            InMode::SKB => write!(f, "skb"),
+        }
+    }
 }
